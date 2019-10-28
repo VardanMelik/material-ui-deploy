d25d0e20a81690dd0e75c1338aa652d33636f3ac
Vardan Meliksetyan <49842390+VardanMelik@users.noreply.github.com>
2019-10-28 21:49:02 +0400

    Add files via upload

---
+++ precache-manifest.49cdc8a69b67d894836ec60b4caf92bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f78de7ea760c8af41f9382ea63a071",
+    "url": "/material-ui-deploy/index.html"
+  },
+  {
+    "revision": "1ef73f2a4be24f367e71",
+    "url": "/material-ui-deploy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9018f86ce3b2d318f618",
+    "url": "/material-ui-deploy/static/js/2.6b5b3b57.chunk.js"
+  },
+  {
+    "revision": "1ef73f2a4be24f367e71",
+    "url": "/material-ui-deploy/static/js/main.59e573ce.chunk.js"
+  },
+  {
+    "revision": "1a102e1c21f2bbd3c075",
+    "url": "/material-ui-deploy/static/js/runtime-main.7c9c267f.js"
+  }
+]);
